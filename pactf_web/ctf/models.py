391898772c5dac1cd7cc1060d2893ce3e5ce8548
Cameron Wong <cam@lancer>
2015-12-21 15:46:02 -0500

    finish a little more setup (mostly empty directories) and some more model changes

--- pactf_web/ctf/models.py
+++ pactf_web/ctf/models.py
@@ -1,10 +1,19 @@
+import importlib.util as iutil
 from django.db import models
 
 class Problem(models.Model):
     p_id = models.AutoField(primary_key=True)
     points = models.IntegerField()
-    # XXX - Better to use FilePathField?
-    grader = models.CharField(help_text='Path to the grading script')
+    name = models.CharField(unique=True, max_length=20)
+    desc = models.TextField()
+    hint = models.TextField(default='')
+    grader = models.FilePathField(help_text='Path to the grading script')
+
+    def grade(self, flag):
+        spec = importlib.util.spec_from_file_location('grader.py', grader)
+        grader = importlib.util.module_from_spec(spec)
+        spec.loader.exec_module(grader)
+        return grader.grade(flag)
 
 class Team(models.Model):
     t_id = models.AutoField(primary_key=True)
--- pactf_web/pactf_web/settings.py
+++ pactf_web/pactf_web/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'ctf',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -76,8 +77,7 @@ WSGI_APPLICATION = 'pactf_web.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
     }
 }
 
