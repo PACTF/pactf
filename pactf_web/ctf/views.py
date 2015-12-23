12013d54ddbc8d6bfc474fd5f1c8ed093bb87663
Cameron Wong <cam@lancer>
2015-12-22 23:47:34 -0500

    finish basic problem viewer backend

--- pactf_web/ctf/views.py
+++ pactf_web/ctf/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, render_to_response
 from django.views import generic
 from django.conf import settings
 
@@ -11,6 +11,9 @@ def get_default_dict(request):
 
 def index(request):
     params = get_default_dict(request)
+    # TODO - Make sure teams can view X problem before it gets put into dict
+    params['prob_list'] = models.Problem.objects.all()
+    return render(request, 'game.html', params)
 
 class TeamDetailView(generic.DetailView):
     model = models.Team
@@ -18,3 +21,5 @@ class TeamDetailView(generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(TeamDetailView, self).get_context_data(**kwargs)
+        context['production'] = not settings.DEBUG
+        return context
