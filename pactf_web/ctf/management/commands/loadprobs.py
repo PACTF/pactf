fe8eab7e82864c192510a6c7bf897ee86ad650f1
Cameron Wong <cam@lancer>
2015-12-23 00:30:33 -0500

    loadprobs management command added

---
+++ pactf_web/ctf/management/commands/loadprobs.py
@@ -0,0 +1,20 @@
+import os, json
+
+from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
+
+from ctf.models import Problem
+
+class Command(BaseCommand):
+    help = (
+        'Scans the directory specified in PROBLEMS_DIR for problem.json '
+        'files. For each one it finds, it creates a new database table '
+        'using the options given.'
+    )
+
+    def handle(self, *args, **options):
+        for root, dirs, files in os.walk(settings.PROBLEMS_DIR):
+            if 'problem.json' in files:
+                data = json.load(open(os.path.join(root, 'problem.json')))
+                p = Problem(**data)
+                p.save()
