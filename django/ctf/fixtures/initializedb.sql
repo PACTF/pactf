3cdf7fc48f39138893ad8374fd82cf254cb338c1
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-24 15:50:57 +0300

    Add script to recreate db for personal reference

---
+++ django/ctf/fixtures/initializedb.sql
@@ -0,0 +1,6 @@
+CREATE DATABASE pactf;
+CREATE USER pactf_user WITH PASSWORD 'pactf_user_password';
+ALTER ROLE pactf_user SET client_encoding TO 'utf8';
+ALTER ROLE pactf_user SET default_transaction_isolation TO 'read committed';
+ALTER ROLE pactf_user SET timezone TO 'UTC';
+GRANT ALL PRIVILEGES ON DATABASE pactf TO pactf_user;
