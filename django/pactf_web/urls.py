1fdcdf95245e79d2382aca6af1d3e1eaf7d02b07
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-24 15:52:04 +0300

    Make settings hackable by django-debug-toolbar

--- django/pactf_web/urls.py
+++ django/pactf_web/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 
-urlpatterns = (
+urlpatterns = [
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'', include('ctf.urls')),
-)
+]
