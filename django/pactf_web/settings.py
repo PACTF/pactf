3f1cb3e6e45e6421997ccbea0658865f6f1e137a
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-24 14:02:16 +0300

    Rewrite dir joining

--- django/pactf_web/settings.py
+++ django/pactf_web/settings.py
@@ -63,7 +63,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            BASE_DIR + '/templates/'
+            os.path.join(BASE_DIR, 'templates'),
         ],
         'APP_DIRS': True,
         'OPTIONS': {
