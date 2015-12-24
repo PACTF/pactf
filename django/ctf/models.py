10787b6a2773af4fe64abb3a34b8cc374c212f6a
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-24 15:53:24 +0300

    Prettify

--- django/ctf/models.py
+++ django/ctf/models.py
@@ -6,8 +6,8 @@ import importlib.util
 import markdown2
 
 
-# TODO - make a JSONField or something similar to store the threshold dict
-class CTFProblem(models.Model):
+# TODO(Cam): make a JSONField or something similar to store the threshold dict
+class CtfProblem(models.Model):
     id = models.AutoField(primary_key=True)
     points = models.IntegerField()
     name = models.CharField(unique=True, max_length=20)
