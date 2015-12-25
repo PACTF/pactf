77baecb025765d0f44680a80d9c24fd52f0bcd46
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-25 19:39:43 +0300

    Add verification of HTTP method verb

--- django/ctf/views.py
+++ django/ctf/views.py
@@ -1,3 +1,6 @@
+import inspect
+
+from django.http.response import HttpResponseNotAllowed
 from django.shortcuts import render, render_to_response
 from django.views.generic import DetailView
 from django.conf import settings
@@ -5,24 +8,69 @@ from django.conf import settings
 from ctf import models
 
 
+# region Helpers
+
 def get_default_dict(request):
     result = {}
     result['production'] = not settings.DEBUG
     return result
 
 
+def http_method(method):
+    """Decorates views to check for HTTP method"""
+    assert method in ('GET', 'POST', 'PUT', 'DELETE')
+    error = HttpResponseNotAllowed('Only {} requests allowed here'.format(method))
+
+    def decorator(view):
+
+        # If view is a class
+        if inspect.isclass(view):
+            old_get = view.get
+
+            def new_get(self, request, *args, **kwargs):
+                if request.method != method:
+                    return error
+                return old_get(self, request, *args, **kwargs)
+
+            view.get = new_get
+
+            return view
+
+        # if view is a function
+        else:
+            def decorated(request):
+                if request.method != method:
+                    return error
+                return view(request)
+
+            return decorated
+
+    return decorator
+
+
+# endregion
+
+
+# region GETs
+
+@http_method('GET')
 def index(request):
+    if request.META.get('REQUEST_METHOD') != 'GET':
+        return HttpResponseNotAllowed('Only GET allowed')
+
     return render(request, 'ctf/index.html')
 
+
+@http_method('GET')
 def game(request):
     params = get_default_dict(request)
-    # TODO(Cam): Make sure teams can view the problem before it gets put into dict
-    # Yatharth: incremental revelation is for the beta; we can use a manager then
     params['prob_list'] = models.CtfProblem.objects.all
-    # FIXME(yatharth): return from session or whatever shiz
+    # TODO(yatharth): return from session or whatever shit
     params['team'] = models.Team.objects.get(id=1)
     return render(request, 'ctf/game.html', params)
 
+
+@http_method('GET')
 class TeamDetailView(DetailView):
     model = models.Team
     template_name = 'ctf/team.html'
@@ -32,5 +80,6 @@ class TeamDetailView(DetailView):
         context.update(get_default_dict(self.request))
         return context
 
-    def get(self, request, *args, **kwargs):
-        return super().get(request, *args, **kwargs)
+
+# endregion
+
