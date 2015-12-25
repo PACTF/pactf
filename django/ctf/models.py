f577166fc27b136534f5659cf8d56427240e8eef
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-25 15:48:03 +0300

    Improve models

--- django/ctf/models.py
+++ django/ctf/models.py
@@ -9,14 +9,20 @@ import markdown2
 # TODO(Cam): make a JSONField or something similar to store the threshold dict
 class CtfProblem(models.Model):
     id = models.AutoField(primary_key=True)
-    points = models.IntegerField()
     name = models.CharField(unique=True, max_length=20)
+
+    points = models.IntegerField()
+
     description = models.TextField()
+    description_html = models.TextField(editable=False)
     hint = models.TextField(default='')
+    hint_html = models.TextField(editable=False, blank=True, null=True)
+
     grader = models.FilePathField(
         help_text='Path to the grading script',
-        path=settings.PROBLEMS_DIR, recursive=True, match='*.py'
+        path=settings.PROBLEMS_DIR, recursive=True, match=r'.*\.py'
     )
+
 
     def grade(self, flag):
         spec = importlib.util.spec_from_file_location('grader.py', self.grader)
@@ -25,12 +31,15 @@ class CtfProblem(models.Model):
         return grader.grade(flag)
 
     def save(self, **kwargs):
+        EXTRAS = ('fenced-code-blocks', 'smarty-pants', 'spoiler')
+
         self.description_html = markdown2.markdown(
-            self.description, extras=['fenced-code-blocks']
+            self.description, extras=EXTRAS
         )
         self.hint_html = markdown2.markdown(
-            self.hint, extras=['fenced-code-blocks']
+            self.hint, extras=EXTRAS
         )
+
         self.full_clean()
         super().save(**kwargs)
 
