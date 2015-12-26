786d8a4d6c28b18cbabcfd00e16fe548989221ad
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-26 11:57:54 +0300

    Add back local_settings.py

---
+++ django/pactf_web/local_settings.py
@@ -0,0 +1,12 @@
+"""Git-ignored file for local settings
+"""
+
+
+TEST_DATABASE = {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': '',
+    'USER': '',
+    'PASSWORD': '',
+    'HOST': '',
+    'PORT': '',
+}
