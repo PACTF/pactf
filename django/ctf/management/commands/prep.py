931a0953fbbe87ff8cdfeaffe07db1fd012eb8c0
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-26 18:41:10 +0300

    Add command to prep

---
+++ django/ctf/management/commands/prep.py
@@ -0,0 +1,12 @@
+from django.core import management
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = "Runs makemigrations, migrate, loadprobs, collectstatic"
+
+    def handle(self, **options):
+        management.call_command('makemigrations')
+        management.call_command('migrate')
+        management.call_command('loadprobs')
+        management.call_command('collectstatic')
