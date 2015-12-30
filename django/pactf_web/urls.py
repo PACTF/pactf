e99a8911b531e968a4c9a362b2d83e890adfa53e
Yatharth Agarwal <yatharth999@gmail.com>
2015-12-30 12:04:14 +0300

    Eliminate warning about string constants

--- django/pactf_web/urls.py
+++ django/pactf_web/urls.py
@@ -1,15 +1,17 @@
 """URL Config
 """
 
+from django.contrib.auth import urls as auth_urls
+from django.contrib.auth import views as auth_views
 from django.conf.urls import include, url
 from django.contrib import admin
 
 
 urlpatterns = [
     # TODO(Yatharth): Write custom templates (then move these rules to ctf/urls.py)
-    url(r'^login/$', 'django.contrib.auth.views.login'),
-    url(r'^logout/$', 'django.contrib.auth.views.logout', {'next_page': 'ctf:index'}),
-    url('^', include('django.contrib.auth.urls')),
+    url(r'^login/$', auth_views.login),
+    url(r'^logout/$', auth_views.logout, {'next_page': 'ctf:index'}),
+    url('^', include(auth_urls)),
 
     url(r'^admin/', include(admin.site.urls), name='admin'),
     url(r'', include('ctf.urls')),
