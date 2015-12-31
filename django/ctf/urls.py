e6f991f22bccac86993d3162b463264cfda274c7
Cameron Wong <cam@lancer>
2015-12-31 03:08:25 -0500

    fix reverse issue from switching to uuid

--- django/ctf/urls.py
+++ django/ctf/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 
 from . import views
 
+UUID_REGEX = "[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}"
 
 app_name = 'ctf'
 
@@ -10,5 +11,5 @@ urlpatterns = [
     url(r'^game$', views.game, name='game'),
     url(r'^team$', views.CurrentTeam.as_view(), name='current_team'),
     url(r'^team/(?P<pk>\d+)$', views.Team.as_view(), name='team'),
-    url(r'^submit_flag/(\d+)', views.submit_flag, name='submit_flag'),
+    url(r'^submit_flag/({})'.format(UUID_REGEX), views.submit_flag, name='submit_flag'),
 ]
