1f456555e686d7b01ac9a6535675f98ddc44606c
Cameron Wong <cam@lancer>
2016-01-12 15:48:23 -0500

    scoreboard updates

--- django/ctf/models.py
+++ django/ctf/models.py
@@ -18,9 +18,13 @@ import markdown2
 # region User Models (by wrapping)
 
 class Team(models.Model):
+    # Essential data
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=40, unique=True)
     score = models.IntegerField(default=0)
+
+    # Nonessential data
+    school = models.CharField(max_length=40, blank=True, default='None')
 
     def __str__(self):
         return "<Team #{} {!r}>".format(self.id, self.name)
