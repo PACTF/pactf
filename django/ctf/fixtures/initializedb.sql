8d6b87c9e52fef7dfa8f3c4d56152dc82ba04f4b
Yatharth Agarwal <yatharth999@gmail.com>
2016-01-12 09:46:18 -0500

    Make manage.py reset_db work

--- django/ctf/fixtures/initializedb.sql
+++ django/ctf/fixtures/initializedb.sql
@@ -6,4 +6,8 @@ ALTER ROLE pactf_user SET client_encoding TO 'utf8';
 ALTER ROLE pactf_user SET default_transaction_isolation TO 'read committed';
 ALTER ROLE pactf_user SET timezone TO 'UTC';
 GRANT ALL PRIVILEGES ON DATABASE pactf TO pactf_user;
+ALTER DATABASE pactf OWNER TO pactf_user;
 -- ALTER USER pactf_user CREATEDB; -- so we can use `manage.py reset_db`
+
+
+
