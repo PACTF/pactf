01b9540896b194de284241a67a40084b75d9a56b
Yatharth Agarwal <yatharth999@gmail.com>
2016-02-26 17:49:57 -0500

    Make loading problems atomic

--- django/ctflex/management/commands/loadprobs.py
+++ django/ctflex/management/commands/loadprobs.py
@@ -10,6 +10,7 @@ import yaml
 from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand, CommandError
+from django.db import transaction
 from django.core import management
 
 from ctflex.constants import (UUID_REGEX, PROBLEM_BASENAME, GRADER_BASENAME, GENERATOR_BASENAME,
@@ -25,7 +26,7 @@ PK_FIELD = 'id'
 
 
 class Command(BaseCommand):
-    help = "Add/Update/Delete problems (and static files)"
+    help = "Add/Update/Delete problems atomically (with static files)"
 
     def add_arguments(self, parser):
         add_no_input_argument(parser)
@@ -94,143 +95,146 @@ class Command(BaseCommand):
             shutil.rmtree(PROBLEMS_STATIC_DIR)
         os.makedirs(PROBLEMS_STATIC_DIR, exist_ok=True)
 
-        # Rotate over window folders
-        for window_basename, window_path in self.walk(PROBLEMS_DIR):
-            for prob_basename, prob_path in self.walk(window_path):
+        with transaction.atomic():
 
-                # Load problem file
-                problem_filename = join(prob_path, PROBLEM_BASENAME)
-                try:
-                    with open(problem_filename) as problem_file:
-                        data = yaml.load(problem_file)
-                except (IsADirectoryError, FileNotFoundError) as err:
-                    write("Skipping '{}': No problems file found".format(prob_basename))
-                    self.handle_error(err)
-                    continue
+            # Rotate over window folders
+            for window_basename, window_path in self.walk(PROBLEMS_DIR):
+                for prob_basename, prob_path in self.walk(window_path):
 
-                # Set paths
-                data['grader'] = join(prob_path, GRADER_BASENAME)
-                if data.get('dynamic', False):
-                    data['dynamic'] = join(prob_path, GENERATOR_BASENAME)
+                    # Load problem file
+                    problem_filename = join(prob_path, PROBLEM_BASENAME)
+                    try:
+                        with open(problem_filename) as problem_file:
+                            data = yaml.load(problem_file)
+                    except (IsADirectoryError, FileNotFoundError) as err:
+                        write("Skipping '{}': No problems file found".format(prob_basename))
+                        self.handle_error(err)
+                        continue
 
-                # Clean and warn about integer IDs
-                if 'id' in data:
-                    self.stderr.write(textwrap.dedent("""\
-                        Warning: Integer IDs are obsolete and will be ignored.
-                        Create/modify a {} file in folder '{}' instead.\
-                        """.format(UUID_BASENAME, prob_basename)))
-                    del data['id']
+                    # Set paths
+                    data['grader'] = join(prob_path, GRADER_BASENAME)
+                    if data.get('dynamic', False):
+                        data['dynamic'] = join(prob_path, GENERATOR_BASENAME)
 
-                # Check for and validate existing UUID file
-                uuid_path = join(prob_path, UUID_BASENAME)
-                if isfile(uuid_path):
-                    with open(uuid_path) as uuid_file:
-                        uuid = uuid_file.read().strip()
-                        data[PK_FIELD] = uuid
+                    # Clean and warn about integer IDs
+                    if 'id' in data:
+                        self.stderr.write(textwrap.dedent("""\
+                            Warning: Integer IDs are obsolete and will be ignored.
+                            Create/modify a {} file in folder '{}' instead.\
+                            """.format(UUID_BASENAME, prob_basename)))
+                        del data['id']
 
-                    if not re.match('{}$'.format(UUID_REGEX), uuid):
-                        write("Error: UUID File did not match the expected format '{}'".format(UUID_REGEX))
+                    # Check for and validate existing UUID file
+                    uuid_path = join(prob_path, UUID_BASENAME)
+                    if isfile(uuid_path):
+                        with open(uuid_path) as uuid_file:
+                            uuid = uuid_file.read().strip()
+                            data[PK_FIELD] = uuid
+
+                        if not re.match('{}$'.format(UUID_REGEX), uuid):
+                            write("Error: UUID File did not match the expected format '{}'".format(UUID_REGEX))
+                            uuid = None
+
+                            write("Backing up and deleting existing UUID file")
+                            backip_uuid_path = join(prob_path, UUID_BACKUP_BASENAME)
+                            shutil.move(uuid_path, backip_uuid_path)
+                    else:
                         uuid = None
 
-                        write("Backing up and deleting existing UUID file")
-                        backip_uuid_path = join(prob_path, UUID_BACKUP_BASENAME)
-                        shutil.move(uuid_path, backip_uuid_path)
+                    # Add window and add defaults
+                    data['window'] = Window.objects.get(code=window_basename)
+                    data.setdefault('dynamic', None)
+                    data.setdefault('description', '')
+                    data.setdefault('hint', '')
+
+                    # If problem exists, update it
+                    query = CtfProblem.objects.filter(**{PK_FIELD: uuid})
+                    if uuid and query.exists():
+                        write("Trying to update problem for '{}'".format(prob_basename))
+                        problem = query.get()
+                        for attr, value in data.items():
+                            setattr(problem, attr, value)
+
+                    # Otherwise, create a new problem
+                    else:
+                        write("Trying to create problem for '{}'".format(prob_basename))
+                        problem = CtfProblem(**data)
+
+                        # Save the UUID to a file
+                        uuid = str(problem.id)
+                        write("Creating a UUID file for '{}'".format(prob_basename))
+                        with open(uuid_path, 'w') as uuid_file:
+                            uuid_file.write(uuid)
+
+                    # Catch validation errors
+                    try:
+                        problem.save()
+                    except ValidationError as err:
+                        write("Validation failed for '{}'".format(prob_basename))
+                        self.handle_error(err)
+
+                        # Don't delete an existing problem later just because validation failed
+                        processed_problems.append(data['id'])
+
+                        continue
+
+                    # Either way, copy over any static files
+                    try:
+                        static_from = join(prob_path, STATIC_BASENAME)
+                        static_to = join(PROBLEMS_STATIC_DIR, str(uuid))
+
+                        if isdir(static_from):
+                            write("Trying to copy static files from '{}'".format(prob_basename))
+                            shutil.copytree(static_from, static_to)
+
+                    except (shutil.Error, IOError) as err:
+                        write("Unable to copy static files for '{}'".format(prob_basename))
+                        self.handle_error(err)
+                        continue
+
+                    # Also, mark as processed
+                    processed_problems.append(problem.id)
+
+                    # We made it!
+                    write("Successfully imported problem for '{}'".format(prob_basename))
+
+            # Delete existing problems that were not updated if clear option was given
+            # (This action is so dangerous that even passing in '--no-input' shouldn't automatically approve it.)
+            unprocessed_problems = CtfProblem.objects.exclude(pk__in=processed_problems).all()
+            if options['clear'] and unprocessed_problems:
+                affirmative_answer = "yes_this_is_dangerous"
+                message = textwrap.dedent("""\
+                    You have requested to delete all pre-existing problems that were not updated.
+                    Please review the list of problems to be deleted.
+
+                        {}
+
+                    This will DELETE ALL THE LISTED PROBLEMS!
+                    Are you sure you want to do this?
+
+                    Type {!r} to continue, or 'no' to cancel:\
+                    """.format(', '.join(map(str, unprocessed_problems)), affirmative_answer))
+                if not options['interactive']:
+                    self.stderr.write("WARNING: You can only delete pre-existing problems in interactive mode\n"
+                                      "(i.e., without the --no-input option)")
+                elif input(message) != affirmative_answer:
+                    self.stderr.write("Did not receive response {!r}; therefore:\n"
+                                      "NOT deleting the above-listed problems"
+                                      .format(affirmative_answer))
                 else:
-                    uuid = None
+                    write("\nDeleting all unprocessed problems\n\n")
+                    for problem in unprocessed_problems:
+                        problem.delete()
 
-                # Add window and add defaults
-                data['window'] = Window.objects.get(code=window_basename)
-                data.setdefault('dynamic', None)
-                data.setdefault('description', '')
-                data.setdefault('hint', '')
+            if self.errored:
+                write()
+                raise RuntimeError(
+                    "Exceptions were encountered; rolling back all database transactions")
 
-                # If problem exists, update it
-                query = CtfProblem.objects.filter(**{PK_FIELD: uuid})
-                if uuid and query.exists():
-                    write("Trying to update problem for '{}'".format(prob_basename))
-                    problem = query.get()
-                    for attr, value in data.items():
-                        setattr(problem, attr, value)
-
-                # Otherwise, create a new problem
-                else:
-                    write("Trying to create problem for '{}'".format(prob_basename))
-                    problem = CtfProblem(**data)
-
-                    # Save the UUID to a file
-                    uuid = str(problem.id)
-                    write("Creating a UUID file for '{}'".format(prob_basename))
-                    with open(uuid_path, 'w') as uuid_file:
-                        uuid_file.write(uuid)
-
-                # Catch validation errors
-                try:
-                    problem.save()
-                except ValidationError as err:
-                    write("Validation failed for '{}'".format(prob_basename))
-                    self.handle_error(err)
-
-                    # Don't delete an existing problem later just because validation failed
-                    processed_problems.append(data['id'])
-
-                    continue
-
-                # Either way, copy over any static files
-                try:
-                    static_from = join(prob_path, STATIC_BASENAME)
-                    static_to = join(PROBLEMS_STATIC_DIR, str(uuid))
-
-                    if isdir(static_from):
-                        write("Trying to copy static files from '{}'".format(prob_basename))
-                        shutil.copytree(static_from, static_to)
-
-                except (shutil.Error, IOError) as err:
-                    write("Unable to copy static files for '{}'".format(prob_basename))
-                    self.handle_error(err)
-                    continue
-
-                # Also, mark as processed
-                processed_problems.append(problem.id)
-
-                # We made it!
-                write("Successfully imported problem for '{}'".format(prob_basename))
-
-        # Delete existing problems that were not updated if clear option was given
-        # (This action is so dangerous that even passing in '--no-input' shouldn't automatically approve it.)
-        unprocessed_problems = CtfProblem.objects.exclude(pk__in=processed_problems).all()
-        if options['clear'] and unprocessed_problems:
-            affirmative_answer = "yes_this_is_dangerous"
-            message = textwrap.dedent("""\
-                You have requested to delete all pre-existing problems that were not updated.
-                Please review the list of problems to be deleted.
-
-                    {}
-
-                This will DELETE ALL THE LISTED PROBLEMS!
-                Are you sure you want to do this?
-
-                Type {!r} to continue, or 'no' to cancel:\
-                """.format(', '.join(map(str, unprocessed_problems)), affirmative_answer))
-            if not options['interactive']:
-                self.stderr.write("WARNING: You can only delete pre-existing problems in interactive mode\n"
-                                  "(i.e., without the --no-input option)")
-            elif input(message) != affirmative_answer:
-                self.stderr.write("Did not receive response {!r}; therefore:\n"
-                                  "NOT deleting the above-listed problems"
-                                  .format(affirmative_answer))
-            else:
-                write("\nDeleting all unprocessed problems\n\n")
-                for problem in unprocessed_problems:
-                    problem.delete()
-
-        # Collect all static files to final location
-        write("")
-        write("Collecting static files")
-        management.call_command('collectstatic', *filter_dict({
-            '--no-input': not options['interactive'],
-            '--clear': options['clear'],
-        }))
-
-        # Throw one large error at end if there were any before
-        if self.errored:
-            raise RuntimeError("Exceptions were encountered")
+            # Collect all static files to final location
+            write("")
+            write("Collecting static files to final location")
+            management.call_command('collectstatic', *filter_dict({
+                '--no-input': not options['interactive'],
+                '--clear': options['clear'],
+            }))
