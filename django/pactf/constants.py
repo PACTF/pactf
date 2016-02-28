24c12ecc70dfa204942b440dc54ed08fb00d95c2
Yatharth Agarwal <yatharth999@gmail.com>
2016-02-28 15:41:07 -0500

    Fix Unicorn

--- django/pactf/constants.py
+++ django/pactf/constants.py
@@ -2,6 +2,9 @@
 
 from os.path import abspath, dirname, join
 
+PROJECT_NAME = 'pactf'
+
+
 ENVDIR_PATH = join(dirname(abspath(__file__)), 'envdir')
 DJANGO_DIR = dirname(dirname(abspath(__file__)))
 BASE_DIR = dirname(DJANGO_DIR)
