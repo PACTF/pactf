b279a216631ac81c3790865eace4873720852d26
Yatharth Agarwal <yatharth999@gmail.com>
2016-03-25 22:26:44 -0400

    Ratelimit admin login

--- django/pactf/urls.py
+++ django/pactf/urls.py
@@ -4,6 +4,14 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 
+from ratelimit.decorators import ratelimit
+
+admin.autodiscover()
+admin.site.login = (
+    ratelimit(key='ip', rate='1/s', method='POST', block=True)(
+        ratelimit(key='ip', rate='10/h', method='POST', block=True)(
+            admin.site.login)))
+
 urlpatterns = [
     url(r'^{}/'.format(settings.ADMIN_URL_PATH), include(admin.site.urls), name='admin'),
     url(r'', include('ctflex.urls')),
