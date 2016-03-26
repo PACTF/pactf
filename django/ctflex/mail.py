8a9c801cb16c26af4bb92712668542515b53ea63
Tony Zhaocheng Tan <tony@tonytan.io>
2016-03-26 00:14:23 -0400

    Temporarily disable email confirmation

--- django/ctflex/mail.py
+++ django/ctflex/mail.py
@@ -8,23 +8,25 @@ from ctflex import settings
 
 def confirm_registration(user):
     """Confirm registration with user"""
+    
+    pass
 
-    context = {
-        'user': user,
-        'support_email': settings.SUPPORT_EMAIL,
-        'sitename': settings.SITENAME,
-    }
+    # context = {
+    #     'user': user,
+    #     'support_email': settings.SUPPORT_EMAIL,
+    #     'sitename': settings.SITENAME,
+    # }
 
-    message = render_to_string('ctflex/email/registration.txt', context)
-    # html_message = render_to_string('ctflex/auth/confirm_email.html', context)
-    subject = render_to_string('ctflex/email/registration.subject.txt', context)
+    # message = render_to_string('ctflex/email/registration.txt', context)
+    # # html_message = render_to_string('ctflex/auth/confirm_email.html', context)
+    # subject = render_to_string('ctflex/email/registration.subject.txt', context)
 
-    mail.send_mail(
-        subject=subject,
-        message=message,
-        # html_message=html_message,
+    # mail.send_mail(
+    #     subject=subject,
+    #     message=message,
+    #     # html_message=html_message,
 
-        from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[user.email],
-        fail_silently=False,
-    )
+    #     from_email=settings.DEFAULT_FROM_EMAIL,
+    #     recipient_list=[user.email],
+    #     fail_silently=False,
+    # )
