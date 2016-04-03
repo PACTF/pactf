26af655264b950768875c8e82b71a746258c763e
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-02 21:17:54 -0400

    Reorganize fixtures

---
+++ ctfproblems/reversing/test2/grader.py
@@ -0,0 +1,5 @@
+def grade(key, submission):
+    if submission == 'flag2':
+        return True, 'gg'
+    else:
+        return False, 'riperino pepperino'
---
+++ ctfproblems/reversing/test3/grader.py
@@ -0,0 +1,5 @@
+def grade(key, submission):
+    if submission == 'flag3':
+        return True, 'gg'
+    else:
+        return False, 'riperino pepperino'
