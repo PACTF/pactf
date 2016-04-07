068395f8bc6b2163a4ed7a454c43f63c19b5ec02
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-07 17:12:58 -0400

    Filter banned teams from scoreboard

--- django/ctflex/queries.py
+++ django/ctflex/queries.py
@@ -230,9 +230,9 @@ def _team_ranking_key(window, team_with_score):
 def board(window=None):
     """Return sorted list of eligible teams with their scores"""
     teams_with_score = ((team, _score_in_timer(team=team, window=window))
-                                 for team in models.Team.objects.iterator()
-                                 # if eligible(team)
-                                 )
+                        for team in models.Team.objects.filter(banned=False).iterator()
+                        # if eligible(team)
+                        )
     ranked = sorted(teams_with_score, key=partial(_team_ranking_key, window))
     return ((i + 1, team, score_) for i, (team, score_) in enumerate(ranked))
 
