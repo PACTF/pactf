4d8403d796b0acc715d28eb2391f62c1f2ed5cba
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-08 21:38:30 -0400

    Reduce logging

--- django/ctflex/middleware.py
+++ django/ctflex/middleware.py
@@ -75,10 +75,10 @@ class CloudflareRemoteAddrMiddleware:
         HTTP_CF_CONNECTING_IP = 'HTTP_CF_CONNECTING_IP'
         EMPTY_IPS = ('', "b''")
 
-        logger.debug("middleware.cloudflare: " + "IP is {}".format(
-            request.META.get(REMOTE_ADDR, '')))
+        # logger.debug("middleware.cloudflare: " + "IP is {}".format(
+        #     request.META.get(REMOTE_ADDR, '')))
         if request.META.get(REMOTE_ADDR, '') in EMPTY_IPS:
-            logger.debug("changing IP from {} to {}".format(
-                request.META.get(REMOTE_ADDR, ''),
-                request.META.get(HTTP_CF_CONNECTING_IP, '')))
+            # logger.debug("middleware.cloudflare: " + "changing IP from {} to {}".format(
+            #     request.META.get(REMOTE_ADDR, ''),
+            #     request.META.get(HTTP_CF_CONNECTING_IP, '')))
             request.META[REMOTE_ADDR] = request.META.get(HTTP_CF_CONNECTING_IP, '')
