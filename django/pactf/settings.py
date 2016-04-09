264a7f7d20bcf2ad6ac8aec16d8333b6b923d1f7
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-09 15:37:50 -0400

    Fix setting CTFLEX_PROBLEMS_STATIC_DIR (part 2)

--- django/pactf/settings.py
+++ django/pactf/settings.py
@@ -327,7 +327,7 @@ class _CTFlex(_Django, Configuration):
     ''' Problems and Staticfiles '''
 
     CTFLEX_PROBLEMS_DIR = values.Value(join(BASE_DIR, 'ctfproblems'), environ_prefix=None)
-    CTFLEX_PROBLEMS_STATIC_DIR = values.Value(join(BASE_DIR, 'ctfproblems', '_static'))
+    CTFLEX_PROBLEMS_STATIC_DIR = values.Value(join(BASE_DIR, 'ctfproblems', '_static'), environ_prefix=None)
     CTFLEX_PROBLEMS_STATIC_URL = 'ctfproblems'
 
     @classmethod
