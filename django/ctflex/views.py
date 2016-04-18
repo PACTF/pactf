4617c1a14cb4daca475649b5c2c9dbb5849cc54f
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-18 06:53:15 -0400

    Upgrade to error

--- django/ctflex/views.py
+++ django/ctflex/views.py
@@ -372,7 +372,7 @@ def submit_flag(request, *, prob_id):
         status = ERROR_STATUS
         message = "The flag was empty."
     except:
-        logger.warning("could not grade flag for {}".format(prob_id), exc_info=True)
+        logger.error("could not grade flag for {}".format(prob_id), exc_info=True)
         status = ERROR_STATUS
         message = "Something went wrong; please report this to us if it persists."
     else:
