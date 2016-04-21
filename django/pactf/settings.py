0602691c308110326c298d78d7f6329f205447a5
Yatharth Agarwal <yatharth999@gmail.com>
2016-04-21 10:00:36 -0400

    Fix fake prod settings

--- django/pactf/settings.py
+++ django/pactf/settings.py
@@ -484,8 +484,8 @@ class FakeProd(Prod):
 
     ''' Security '''
 
-    NORECAPTCHA_SITE_KEY = values.Value('6LeF8h0TAAAAAMJrdcK8g7nn1lGVCNbgzskXHj5S', environ_prefix=None)
-    NORECAPTCHA_SECRET_KEY = values.Value('6LeF8h0TAAAAAP_U_TIChL_6y8cTiu2jhTg1cdzG', environ_prefix=None)
+    NORECAPTCHA_SITE_KEY = '6LeF8h0TAAAAAMJrdcK8g7nn1lGVCNbgzskXHj5S'
+    NORECAPTCHA_SECRET_KEY = '6LeF8h0TAAAAAP_U_TIChL_6y8cTiu2jhTg1cdzG'
 
     ''' Logging '''
 
