b218956a83f51d6b48587e222368d39c3f6e1bd6
Yatharth Agarwal <yatharth999@gmail.com>
2017-03-14 22:54:49 -0400

    Revert "restore NORECAPTCHA_VERIFY_URL value"
    This reverts commit ca006866eabe8163163cfa89b5da371acb1627d8.

--- django/pactf/settings.py
+++ django/pactf/settings.py
@@ -443,7 +443,6 @@ class Prod(_Base):
 
     NORECAPTCHA_SITE_KEY = values.Value('6Ldt_h0TAAAAAHZi1Mk455UT0-XNmDkyKoJMH3wW', environ_prefix=None)
     NORECAPTCHA_SECRET_KEY = values.SecretValue(environ_prefix=None)
-    NORECAPTCHA_VERIFY_URL = values.Value('https://www.google.com:61561/recaptcha/api/siteverify', environ_prefix=None)
 
     ''' Logging '''
 
